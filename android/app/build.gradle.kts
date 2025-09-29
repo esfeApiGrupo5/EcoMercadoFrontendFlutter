fdcf12fcc0a9d67329dc751b068d0fed15c85662
vf25002 <vf25002@esfe.agape.edu.sv>
2025-09-29 16:32:39 +0000

    posible solucion en la app mvil para acceso a internet

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapp"
+    namespace = "com.example.EcoMercado"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
